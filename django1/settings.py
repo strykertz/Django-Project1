497bd71ada33a036609cd25456ed434d654d6584
marcusfranco <marcusvgf16@gmail.com>
2021-01-19 22:20:48 -0300

    Projeto Finalizado!

--- django1/settings.py
+++ django1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2p#k21bfv-0a3&^dz!z4l2_lud)gekkuds&dl+u&bxjg5f9-hd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
